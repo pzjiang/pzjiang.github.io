5e676df865ece9c17b8015f50d5208939711ef35
pzjiang <pzjiang01@hotmail.com>
2021-06-21 17:56:40 -0400

    create styling files and framework for blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,21 @@ import * as React from "react"
 import Layout from "../components/layout.js"
 
 
+/*
+    title:
+    date:
+    content:
+*/
+const blogposts = [
+
+    {
+
+
+    },
+
+]
+
+
 const BlogPage = () => {
 
     return (
