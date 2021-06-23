574b62c42b3763e11f1ab1869172ba2d82f69a24
pzjiang <pzjiang01@hotmail.com>
2021-06-23 00:38:11 -0400

    create unstyled content for contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,14 +10,24 @@ const ContactPage = () => {
                 <title>Contact</title>
 
                 <div class="emailsection">
-
+                    <h1>Email</h1>
+                    <p>pzjiang01@hotmail.com</p>
                 </div>
 
                 <div class="wechatsection">
-
+                    <h1>WeChat</h1>
+                    <p> pzjiang4568</p>
                 </div>
 
                 <div class="githubsection">
+
+                    <h1>GitHub</h1>
+                    <p>pzjiang01</p>
+
+                </div>
+                <div class="codeforce">
+                    <h1>CodeForce</h1>
+                    <p>TBD</p>
 
                 </div>
 
