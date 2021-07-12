07b5aa14205424712fe2089db274ce7f95791a30
pzjiang <pzjiang01@hotmail.com>
2021-07-12 15:48:08 -0400

    move footer and add change to links when active

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,24 +8,25 @@ const ContactPage = () => {
 
         <Layout>
             <title>Contact</title>
+            <div class="contactbody">
 
-            <div class="emailsection">
-                <h1>Email</h1>
-                <p>pzjiang01@hotmail.com</p>
+                <div class="emailsection">
+                    <h1>Email</h1>
+                    <p>pzjiang01@hotmail.com</p>
+                </div>
+
+                <div class="wechatsection">
+                    <h1>WeChat</h1>
+                    <p> pzjiang4568</p>
+                </div>
+
+                <div class="githubsection">
+
+                    <h1>GitHub</h1>
+                    <p>pzjiang01</p>
+
+                </div>
             </div>
-
-            <div class="wechatsection">
-                <h1>WeChat</h1>
-                <p> pzjiang4568</p>
-            </div>
-
-            <div class="githubsection">
-
-                <h1>GitHub</h1>
-                <p>pzjiang01</p>
-
-            </div>
-
 
         </Layout>
 
