0f0f0de567ac46d179a3120a72a474264f4795a1
pzjiang <pzjiang01@hotmail.com>
2021-07-19 14:06:05 -0400

    start work on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,15 +7,11 @@ import Layout from "../components/layout.js"
     date:
     content:
 */
-const blogposts = [
 
-    {
-        title: "",
-        date: "",
-        content: "",
-    },
 
-]
+
+
+//styles will be located in the contact page css
 
 
 const AboutPage = () => {
@@ -26,7 +22,8 @@ const AboutPage = () => {
                 <title>About</title>
 
                 <div class="aboutstage">
-
+                    <h1>About Me</h1>
+                    <p>I am a rising junior at Purdue University, studying software engineering and machine learning.</p>
 
                 </div>
 
