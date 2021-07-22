b7a0ebe43afe99b4e9661bb68dd28d5627575fe7
pzjiang <pzjiang01@hotmail.com>
2021-07-22 12:16:29 -0400

    add link

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,10 +29,14 @@ const AboutPage = () => {
                         </div>
 
                         <div class="column">
-                            <p>I Peter Jiang, an undergrad at Purdue University, studying computer science with minors in statistics and chinese.
-                                My main points of interest are software engineering and data structures/algorithms.
+                            <p>My name is Peter Jiang, an undergrad at Purdue University, studying computer science with minors in statistics and chinese.
+                                Software engineering and data structures/algorithms are my main points of interest.
                             </p>
                             <p>
+                                I also teach Hu-Lu-Si, providing both private lessons and public lessons at Chinese Schools. I have taught 5 years collectively at
+                                the Pittsburgh Chinese School located in Pittsburgh, PA, as well as the Carmel branch of ICCCI in Carmel, Indiana. I currently
+                                teach virtually through Zoom for the TAMU Chinese based in College Station, Texas. Check out the syllabus website here:
+                                <Link to="https://hulusi.gatsbyjs.io/">Hu-Lu-Si</Link>
 
                             </p>
                         </div>
@@ -40,7 +44,7 @@ const AboutPage = () => {
 
                 </div>
 
-                <div class="">
+                <div class="knowledge">
 
                 </div>
 
