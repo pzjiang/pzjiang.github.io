15fafaafe28a9072baeb991de688882b306d47e3
pzjiang <pzjiang01@hotmail.com>
2021-09-15 16:08:17 -0400

    Updates

---
+++ component---src-pages-moneybuddy-js-28a13229b8a7a19bc751.js
@@ -0,0 +1,2 @@
+(self.webpackChunkpwebsite=self.webpackChunkpwebsite||[]).push([[620],{2151:function(e,t,n){"use strict";n.r(t);var u=n(7294),l=n(7198);t.default=function(){return u.createElement(l.Z,null,u.createElement("title",null,"M Buddy"))}}}]);
+//# sourceMappingURL=component---src-pages-moneybuddy-js-28a13229b8a7a19bc751.js.map
